be20522d54784b5c9cd55e8e0616b44a1649ca5f
hongdong.liao <hongdong.liao@vantopgroup.com>
2022-05-20 16:44:20 +0800

    rm ip

--- main.cpp
+++ main.cpp
@@ -2,25 +2,19 @@
  * @Author: hongdong.liao
  * @Date: 2022-03-21 10:51:51
  * @LastEditors: hongdong.liao
- * @LastEditTime: 2022-04-26 10:48:50
+ * @LastEditTime: 2022-05-20 16:35:07
  * @FilePath: /test/main.cpp
  */
-// #include "Message.hpp"
+#include "Message.hpp"
 // #include "Student.hpp"
 
-#include "IpAddress.hpp"
-
-// #include <cstdlib>
-// #include <iostream>
+#include <cstdlib>
+#include <iostream>
 
 int main(int argc, const char *argv[])
 {
-    // Message say_hello("Hello, CMake World!");
-    // std::cout << say_hello << std::endl;
-
-    // IpAddress ip;
-    // std::string address = ip.getIp();
-    // std::cout << "ip address: " << address << std::endl;
+    Message say_hello("Hello, CMake World!");
+    std::cout << say_hello << std::endl;
 
     return EXIT_SUCCESS;
 }
