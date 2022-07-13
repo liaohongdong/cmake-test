ba377c5ba277d55a2a1c7993c16f8e5e22b1f4c4
hongdong.liao <hongdong.liao@vantopgroup.com>
2022-07-13 15:32:14 +0800

    MOD-FEAT: wip

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
  * @Author: hongdong.liao
  * @Date: 2022-03-21 10:51:51
  * @LastEditors: hongdong.liao
- * @LastEditTime: 2022-05-20 16:35:07
- * @FilePath: /test/main.cpp
+ * @LastEditTime: 2022-07-12 18:12:56
+ * @FilePath: /cmake-test/main.cpp
  */
 #include "Message.hpp"
 // #include "Student.hpp"
@@ -11,10 +11,18 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(int argc, const char *argv[])
+#include <Python.h>
+
+int main(int argc, char *argv[])
 {
     Message say_hello("Hello, CMake World!");
     std::cout << say_hello << std::endl;
+
+    Py_SetProgramName((wchar_t *)argv[0]);
+    Py_Initialize();
+    PyRun_SimpleString("import random\n"
+                        "print('Today is', random.randint(1, 10000))\n");
+    Py_Finalize();
 
     return EXIT_SUCCESS;
 }
---
+++ use_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def print_ones(rows, cols):
+
+    A = np.ones(shape=(rows, cols), dtype=float)
+    print(A)
+
+    num_elements = rows * cols
+    return(num_elements)
