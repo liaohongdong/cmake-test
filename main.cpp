0dc40aced0bce4b83bde390bcc3fa620b62ff9cc
hongdong.liao <hongdong.liao@vantopgroup.com>
2022-07-14 17:34:12 +0800

    MOD-FEAT: wip

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
  * @Author: hongdong.liao
  * @Date: 2022-03-21 10:51:51
  * @LastEditors: hongdong.liao
- * @LastEditTime: 2022-05-20 16:35:07
- * @FilePath: /test/main.cpp
+ * @LastEditTime: 2022-07-14 15:58:44
+ * @FilePath: /cmake-test/main.cpp
  */
+#include "sum_integers.hpp"
 #include "Message.hpp"
 // #include "Student.hpp"
 
@@ -15,6 +16,14 @@ int main(int argc, const char *argv[])
 {
     Message say_hello("Hello, CMake World!");
     std::cout << say_hello << std::endl;
+
+    std::vector<int> integers;
+    for (auto i = 1; i < argc; i++)
+    {
+        integers.push_back(std::stoi(argv[i]));
+    }
+    auto sum = sum_integers(integers);
+    std::cout << sum << std::endl;
 
     return EXIT_SUCCESS;
 }
