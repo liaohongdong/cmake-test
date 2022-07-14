9ca7b52588bb6ab1a6ddc6d5857a0757f3566214
hongdong.liao <hongdong.liao@vantopgroup.com>
2022-07-14 15:40:17 +0800

    MOD-FEAT: wip

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include "sum_integers.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch/catch.hpp"
+#include <vector>
+
+TEST_CASE("Sum of integers for a short vector", "[short]") {
+    auto integers = { 1, 2, 3, 4, 5 };
+    REQUIRE(sum_integers(integers) == 15);
+}
+
+TEST_CASE("Sum of integers for a longer vector", "[long]") {
+    std::vector<int> integers;
+    for (auto i = 1; i < 1001; i++)
+    {
+        integers.push_back(i);
+    }
+    REQUIRE(sum_integers(integers) == 500500);
+}
